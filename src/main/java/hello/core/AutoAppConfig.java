838873eea69262c4df91e1692b322cd68b2aa870
jamey <musoyou1085@gmail.com>
2024-02-14 23:07:43 +0900

    의존관계 자동 주입

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,22 +1,18 @@
 package hello.core;
 
-import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member", // 어디서 부터 찾는 지 지정
+//        basePackages = "hello.core", // 어디서 부터 찾는 지 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
- ) // 원래는 안함. test code를 지키기 위함.
+) // 원래는 안함. test code를 지키기 위함.
 public class AutoAppConfig {
 
-
-    @Bean(name = "memoryMemberRepository")
-    public MemberRepository memberRepository() {
-       return new MemoryMemberRepository();
-    }
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//       return new MemoryMemberRepository();
+//    }
 }
